c542ff63b23ca16fb00c946c116332bbdbf4f2e5
RushikeshBBankar <rushikesh1611@gmail.com>
2024-04-26 19:05:29 +0530

    I remove comment

--- cypress/e2e/UI/TC1_first.cy.js
+++ cypress/e2e/UI/TC1_first.cy.js
@@ -1,6 +1,5 @@
 /// <reference types="Cypress" />
 
-// This is my first test case
 describe('Check the page navigation',()=>{
         it('Verify url',()=>{
                 cy.visit('https://webdriveruniversity.com/')
